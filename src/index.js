d765e012e110b58d7c7644f1e79b1342f695aeb4
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-02-25 22:45:38 +0200

    add webpackconfig

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.scss';
+import './index.html';
+import { calc, mult } from './modules/calc';
+
+
+
+console.log(mult(2, 4));
+console.log(calc(2, 4));
---
+++ src/modules/calc.js
@@ -0,0 +1,6 @@
+export const mult = (a, b) => {
+  return a * b
+ }
+ export const calc = async (a, b) => {
+  return a + b
+ }
